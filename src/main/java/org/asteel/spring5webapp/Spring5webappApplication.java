e1120acc90d26e414b4c183116fc86848966134b
Andrey Stalnoy <stalnoyandrey.official@gmail.com>
2019-06-07 18:53:10 +0600

    Uploading Project

---
+++ src/main/java/org/asteel/spring5webapp/Spring5webappApplication.java
@@ -0,0 +1,14 @@
+package org.asteel.spring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webappApplication.class, args);
+		System.out.println("Welcome to your first Spring application");
+	}
+
+}
